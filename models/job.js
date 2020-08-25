3855024b100b27679b7e1e2b396fd1dadbdd1af4
Ardita Burhop <kalendarperaden@gmail.com>
2020-08-25 14:58:57 -0500

    testtables

--- models/job.js
+++ models/job.js
@@ -2,14 +2,33 @@ const Sequelize = require("sequelize");
 const db = require("./db")
 
 const Job = db.define("jobs", {
+  company:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   jobTitle:{
     type: Sequelize.STRING,
     allowNull: false
   },
+  jobNotes:{
+    type: Sequelize.TEXT,
+  },
   jobPostUrl:{
     type: Sequelize.STRING,
     allowNull: false
-  }
+  },
+  location: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  deadline:{
+    type: Sequelize.DATEONLY,
+    allowNull: false
+  },
 });
 
 module.exports = Job
