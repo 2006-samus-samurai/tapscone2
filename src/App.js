a191e76b7671af248bb1d2527bded57a199e6969
Ardita Burhop <kalendarperaden@gmail.com>
2020-08-28 16:03:07 -0500

    commit3

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Navbar} from './components'
+import Routes from './routes'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div id="main-page-content">
+        <Routes />
+      </div>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {Router} from 'react-router-dom'
+import history from './history'
+import store from './store'
+import App from './app'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <Router history={history}>
       <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+)
