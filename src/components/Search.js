1a62a91cdfcf430fb0668906e4dd3e254e382699
Ardita Burhop <kalendarperaden@gmail.com>
2020-08-28 15:09:08 -0500

    commit2

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,31 @@
 import React from 'react';
-
+import axios from "axios"
 import dummyData2 from './dummyData2';
 
 class Search extends React.Component {
-  state = dummyData2;
+  // state = dummyData2;
+  constructor(props) {
+    super(props)
+    this.state = {
+      jobs: []
+    }
 
+  }
+  async componentDidMount() {
+    try {
+
+      const {data} = await axios.get(`/api/jobs/`)
+      this.setState({
+        jobs: data
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  //state = dummyData2;
   render() {
-    const jobs = this.state;
+    const {jobs} = this.state;
     return (
       <div>
         <div class="md-form mt-0">
